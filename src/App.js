994d0c63e7f289c231cd72515f86954a9a581a54
sharopcha <napster1202@gmail.com>
2020-08-15 02:56:46 +0300

    dashboard and project list layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
+import Dashboard from './components/dashboard/Dashboard';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Navbar />
+        <Switch>
+          <Route path="/" component={Dashboard}></Route>
+        </Switch>
       </div>
     </Router>
   );
---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ProjectSummary = () => {
+  return (
+    <div className="card z-depth-1 project-summary">
+      <div className="card-content text-grey text-darken-3">
+        <span className="card-title">Project Title</span>
+        <p>Posted by Brayn Adams</p>
+        <p className="grey-text">3rd September. 2am</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectSummary;
