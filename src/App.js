9794364d5b9c42279e36286b83a711ebacdf2cb3
sharopcha <napster1202@gmail.com>
2020-08-15 02:30:43 +0300

    setup materialize-css and navbar layout

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/layout/SignedInLinks.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const SignedInLinks = () => {
+  return (
+    <ul className="right">
+      <li>
+        <NavLink to="/">New project</NavLink>
+      </li>
+      <li>
+        <NavLink to="/">Log out</NavLink>
+      </li>
+      <li>
+        <NavLink to="/" className="btn btn-floating pink lighten-1">
+          NN
+        </NavLink>
+      </li>
+    </ul>
+  );
+};
+
+export default SignedInLinks;
